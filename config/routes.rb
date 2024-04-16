653d8f6d48e8b98786e79661112bb7ddcb33fbe9
EC2 Default User <ec2-user@ip-172-31-23-107.ap-northeast-1.compute.internal>
2024-04-16 19:25:53 +0000

    meshiterro add

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   root 'homes#top'
 
-  resources :post_images, only: [:new,  :create, :index, :show]
-
   get 'homes/about', to: 'homes#about', as: :about
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
 end
